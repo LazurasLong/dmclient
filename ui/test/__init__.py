2658b7809e555ac93cdc9cdbb2dc372f4637acd2
Alex <alex@theshrine.ca>
2017-06-10 22:30:41 -0600

    ui/test: allow module under test to define its own QMainWindow subclass

--- ui/test/__init__.py
+++ ui/test/__init__.py
@@ -11,21 +11,24 @@ app = None
 
 
 def main():
+    global app
     parser = argparse.ArgumentParser()
     parser.add_argument("module_name")
     args = parser.parse_args(sys.argv[1:])
     logging.basicConfig(level=logging.DEBUG)
     try:
         app_name = "dmclient test harness"
-        global app
         app = QApplication(sys.argv)
         app.setApplicationName(app_name)
 
-        main_window = QMainWindow()
-        main_window.setWindowTitle(app_name)
         import ui.widgets.icons_rc
 
         ns = __import__(args.module_name, globals(), locals(), ('guitest_main',))
+
+        main_window_cls = getattr(ns, "MainWindow", QMainWindow)
+        main_window = main_window_cls()
+        main_window.setWindowTitle(app_name)
+
         guitest_main = ns.guitest_main
         guitest_main(main_window)
 
